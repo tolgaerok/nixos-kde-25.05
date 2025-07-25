4876ca956fcc44fa27ff6b220744887a1a51e25f
Tolga Erok <kingtolga@gmail.com>
2025-07-25 18:13:08 +0800

    (ツ)_/¯ Edit: 25-07-2025 06:13:08 PM

--- flatpaks/linuxtweaks-flatpak-update-copy.sh
+++ flatpaks/linuxtweaks-flatpak-update-copy.sh
@@ -1,5 +1,10 @@
 #!/run/current-system/sw/bin/bash
 export PATH=/run/current-system/sw/bin:$PATH
+
+DISPLAY=:0
+DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+XDG_RUNTIME_DIR=/run/user/1000
+real_user=${username}
 sleep 10
 
 icon="/etc/nixos/flatpaks/LinuxTweak.png"
@@ -8,8 +13,17 @@ log() {
     printf "\e[34m[INFO] %s\e[0m\n" "$1"
 }
 
+#notify() {
+#    /run/current-system/sw/bin/notify-send "$1" "$2" --app-name="$3" -i "$icon" -u NORMAL
+#}
+
 notify() {
-    /run/current-system/sw/bin/notify-send "$1" "$2" --app-name="$3" -i "$icon" -u NORMAL
+    /run/current-system/sw/bin/notify-send \
+      "$1" "$2" \
+      --app-name="$3" \
+      --icon="$icon" \
+      --urgency=normal \
+      --expire-time=5000
 }
 
 sleep 5
